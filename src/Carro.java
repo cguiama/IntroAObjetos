b7c0059486e9f5d8c9527422f8ef2b3646f372df
Guilherme <45200933+cguiama@users.noreply.github.com>
2022-01-27 11:16:11 -0300

    Criado objeto Carro e inputado seus valores.

--- src/Carro.java
+++ src/Carro.java
@@ -1,5 +1,55 @@
 /**
  * Classe carro
  */
-public class Carro {
+
+class Carro {
+
+    String cor;
+    String modelo;
+    int capacidadeTanque;
+
+    //construtor
+
+    Carro(){
+    }
+
+    //sobrecargas
+
+    Carro(String cor, String modelo, int capacidadeTanque){
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    String getCor() {
+        return cor;
+    }
+
+    void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    String getModelo() {
+        return modelo;
+    }
+
+    void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+
+    }
+
+    int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    //metodos
+
+    double totalValorTanque(double valorCombustivel) {
+        return capacidadeTanque * valorCombustivel;
+    }
+
 }
